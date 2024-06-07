b1cfbc491810617eb52afd7b98eb4bed16e92ba9
juandanieldev29 <juandanieldev29@gmail.com>
2024-06-07 09:56:35 -0600

    adding error handling for the auth service (#4)

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -4,9 +4,11 @@ import { json } from 'body-parser';
 import cors from 'cors';
 import cookieSession from 'cookie-session';
 
+import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,5 +19,11 @@ app.use(cookieSession({ signed: false, secure: false }));
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 export { app };
