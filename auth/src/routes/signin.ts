2b2c373891738b1734cffdf083c15810216ff59f
Juan Daniel <juandanieldev29@gmail.com>
2024-06-07 09:55:06 -0600

    adding error handling for the auth service

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,16 +3,19 @@ import { body } from 'express-validator';
 
 import { googleOAuthClient } from '../services/google-oauth-client';
 import { getGoogleOAuthProfile } from '../services/google-oauth-profile';
+import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
   '/api/auth/google',
   [body('code').trim().notEmpty().withMessage('You must provide a code')],
+  validateRequest,
   async (req: Request, res: Response) => {
     const { tokens } = await googleOAuthClient.getToken(req.body.code);
     if (!tokens.id_token) {
-      throw new Error('ID Token not present in google oAuth response');
+      throw new BadRequestError('ID Token not present in google oAuth response');
     }
     const currentUser = await getGoogleOAuthProfile(tokens.id_token);
     req.session = {
--- auth/src/services/google-oauth-profile.ts
+++ auth/src/services/google-oauth-profile.ts
@@ -2,6 +2,7 @@ import { UserRefreshClient } from 'google-auth-library';
 
 import { googleOAuthClient } from './google-oauth-client';
 import { UserProfile } from '../types/user-profile';
+import { BadRequestError } from '../errors/bad-request-error';
 
 export const getGoogleOAuthProfile = async (idToken: string): Promise<UserProfile> => {
   const googleOAuth = await googleOAuthClient.verifyIdToken({
@@ -11,7 +12,7 @@ export const getGoogleOAuthProfile = async (idToken: string): Promise<UserProfil
   const userId = googleOAuth.getUserId();
   const userInfo = googleOAuth.getPayload();
   if (!userInfo?.email || !userInfo?.name || !userId) {
-    throw new Error('Could not retrieve user profile info');
+    throw new BadRequestError('Could not retrieve user profile info from google');
   }
   return {
     id: userId,
@@ -29,7 +30,7 @@ export const refreshGoogleToken = async (refreshToken: string): Promise<UserProf
   );
   const { credentials } = await user.refreshAccessToken();
   if (!credentials.id_token) {
-    throw new Error('ID Token not present in google oAuth response');
+    throw new BadRequestError('ID Token not present in google oAuth response');
   }
   return await getGoogleOAuthProfile(credentials.id_token);
 };
