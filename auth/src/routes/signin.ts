868e5c8b9e3240304ef0d0e48577d576fe111a1a
juandanieldev29 <juandanieldev29@gmail.com>
2024-06-07 09:28:31 -0600

    reusing existing logic in sign in route (#3)

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 
 import { googleOAuthClient } from '../services/google-oauth-client';
+import { getGoogleOAuthProfile } from '../services/google-oauth-profile';
 
 const router = express.Router();
 
@@ -11,23 +12,9 @@ router.post(
   async (req: Request, res: Response) => {
     const { tokens } = await googleOAuthClient.getToken(req.body.code);
     if (!tokens.id_token) {
-      throw new Error('Could not retrieve google token');
+      throw new Error('ID Token not present in google oAuth response');
     }
-    const googleOAuth = await googleOAuthClient.verifyIdToken({
-      idToken: tokens.id_token,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
-    const userId = googleOAuth.getUserId();
-    const userInfo = googleOAuth.getPayload();
-    if (!userInfo?.email || !userInfo?.name || !userId) {
-      throw new Error('Could not retrieve user profile info');
-    }
-    const currentUser = {
-      id: userId,
-      email: userInfo.email,
-      name: userInfo.name,
-      photoURL: userInfo.picture,
-    };
+    const currentUser = await getGoogleOAuthProfile(tokens.id_token);
     req.session = {
       idToken: tokens.id_token,
       accessToken: tokens.access_token,
