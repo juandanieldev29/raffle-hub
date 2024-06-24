01514ce5a5bb1b4df9e103b3fa67eeb08ff4dc87
juandanieldev29 <juandanieldev29@gmail.com>
2024-06-24 15:53:45 -0600

    fixing current user middleware

--- raffles/src/middlewares/current-user.ts
+++ raffles/src/middlewares/current-user.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-import { getGoogleOAuthProfile, refreshGoogleToken } from '../services/google-oauth-profile';
+import {
+  verifyIdToken,
+  refreshCredentials,
+  fetchProfileFromPayload,
+  fetchProfileFromGoogle,
+} from '../services/google-oauth';
 import { UserProfile } from '../types/user-profile';
 
 declare global {
@@ -12,20 +17,63 @@ declare global {
   }
 }
 
+interface Credentials {
+  idToken: string;
+  accessToken: string;
+  refreshToken: string;
+  expiryDate: number;
+}
+
 export const currentUser = async (req: Request, _res: Response, next: NextFunction) => {
-  if (!req.session?.idToken) {
+  if (
+    !req.session?.idToken ||
+    !req.session?.accessToken ||
+    !req.session?.refreshToken ||
+    !req.session?.expiryDate
+  ) {
+    req.session = null;
     return next();
   }
+  let credentials: Credentials = {
+    idToken: req.session.idToken,
+    accessToken: req.session.accessToken,
+    refreshToken: req.session.refreshToken,
+    expiryDate: req.session.expiryDate,
+  };
   try {
-    const tokenExpirationTime = req.session.expiryDate;
+    const tokenExpirationTime = credentials.expiryDate;
     const currentTimestamp = Date.now();
     if (currentTimestamp > tokenExpirationTime) {
-      if (req.session.refreshToken) {
-        req.currentUser = await refreshGoogleToken(req.session.refreshToken);
+      console.log('Refreshing Google OAuth token');
+      const refreshedCredentials = await refreshCredentials(req.session.refreshToken);
+      if (
+        !refreshedCredentials.id_token ||
+        !refreshedCredentials.access_token ||
+        !refreshedCredentials.refresh_token ||
+        !refreshedCredentials.expiry_date
+      ) {
+        req.session = null;
+        return next();
       }
-    } else {
-      req.currentUser = await getGoogleOAuthProfile(req.session.idToken);
+      credentials = {
+        idToken: refreshedCredentials.id_token,
+        accessToken: refreshedCredentials.access_token,
+        refreshToken: refreshedCredentials.refresh_token,
+        expiryDate: refreshedCredentials.expiry_date,
+      };
     }
+    const loginTicket = await verifyIdToken(credentials.idToken);
+    if (req.session.currentUser && req.session.currentUser.id === loginTicket.getUserId()) {
+      return next();
+    }
+    const payloadUserProfile = fetchProfileFromPayload(loginTicket);
+    if (payloadUserProfile) {
+      req.session = { ...req.session, currentUser: payloadUserProfile };
+      return next();
+    }
+    const googleUserProfile = await fetchProfileFromGoogle(credentials.accessToken);
+    req.session = { ...req.session, currentUser: googleUserProfile };
+    return next();
   } catch (err) {
     console.log(err);
     req.session = null;
