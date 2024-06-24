c8d23eba8f8d9494ae3691d7ec178600c76b9e33
juandanieldev29 <juandanieldev29@gmail.com>
2024-06-24 15:52:40 -0600

    adding pagination to endpoint to retrieve all raffles

--- raffles/src/routes/index.ts
+++ raffles/src/routes/index.ts
@@ -4,11 +4,22 @@ import { Raffle } from '../models/raffle';
 
 const router = express.Router();
 
-router.get('/api/raffles', async (_req: Request, res: Response) => {
-  const raffles = await Raffle.find({
-    complete: false,
-  });
-  res.send(raffles);
+interface Pagination {
+  page?: number;
+  pageSize?: number;
+}
+
+router.get('/api/raffles', async (req: Request<{}, {}, {}, Pagination>, res: Response) => {
+  const { page = 1, pageSize = 10 } = req.query;
+  const [raffles, count] = await Promise.all([
+    Raffle.find({
+      complete: false,
+    })
+      .skip((page - 1) * pageSize)
+      .limit(pageSize),
+    Raffle.countDocuments(),
+  ]);
+  res.send({ raffles, metadata: { count } });
 });
 
 export { router as indexRaffleRouter };
